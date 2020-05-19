88b443f7bde7366401e10bb347458a1f7e681e71
Walid Baroudi <47188367+waleedbaroudi@users.noreply.github.com>
2020-05-19 18:52:58 +0300

    Update HeapSort.java

--- HeapSort.java
+++ HeapSort.java
@@ -2,20 +2,11 @@ package code;
 
 import given.AbstractArraySort;
 
-/*
- * Implement the heap-sort algorithm here. You can look at the slides for the pseudo-code.
- * Make sure to use the swap and compare functions given in the AbstractArraySort!
- * 
- */
 
 public class HeapSort<K extends Comparable<K>> extends AbstractArraySort<K> {
 
-	// Add any fields here
-
 	public HeapSort() {
 		name = "Heapsort";
-
-		// Initialize anything else here
 	}
 
 	@Override
@@ -28,15 +19,12 @@ public class HeapSort<K extends Comparable<K>> extends AbstractArraySort<K> {
 		}
 	}
 
-	// Public since we are going to check its output!
 	public void heapify(K[] inputArray) {
 		for (int i = inputArray.length / 2; i >= 0; i--) {
 			downheap(inputArray, i, inputArray.length - 1);
 		}
 	}
 
-	// The below methods are given given as suggestion. You do not need to use them.
-	// Feel free to add more methods
 
 	protected void downheap(K[] inputArray, int i, int endInd) {
 		int current = i, left = 2 * i + 1, right = 2 * i + 2;
